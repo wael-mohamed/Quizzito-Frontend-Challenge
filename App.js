1801a4f7e169c8e6026ceda98be566188e5a2724
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-08 22:00:12 +0100

    make image on the very top

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeScreen from './screens/HomeScreen';
-import Screen2 from './screens/Screen2';
-import Screen3 from './screens/Screen3';
+import HomeScreen from "./screens/HomeScreen";
+import Screen2 from "./screens/Screen2";
+import Screen3 from "./screens/Screen3";
 
 const Stack = createStackNavigator();
 
@@ -13,7 +13,13 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Second" component={Screen2} />
+        <Stack.Screen
+          name="Second"
+          component={Screen2}
+          options={{
+            header: () => null,
+          }}
+        />
         <Stack.Screen name="Third" component={Screen3} />
       </Stack.Navigator>
     </NavigationContainer>
