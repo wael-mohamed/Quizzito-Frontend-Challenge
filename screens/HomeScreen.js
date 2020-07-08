b97a0dc30292b15206650d7437a640842a19d688
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-08 19:08:15 +0100

    refactor HomeScreen Button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,41 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import * as React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { StyledButton } from "../components/Button";
 
 export default function HomeScreen({ navigation }) {
-  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the first Quizzito Frontend Challenge !</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Second')}
-      >
-        <Text style={styles.buttonText}>Go to the Next screen</Text>
-      </TouchableOpacity>
+      <Text style={styles.title}>
+        Welcome to the first Quizzito Frontend Challenge !
+      </Text>
+      <StyledButton onPress={() => navigation.navigate("Second")}>
+        Go to the Next screen
+      </StyledButton>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    alignItems: "center",
+    justifyContent: "space-around",
     flex: 1,
-    paddingHorizontal:40,
-    backgroundColor:'#f9f9f9',
+    paddingHorizontal: 40,
+    backgroundColor: "#f9f9f9",
   },
-  title:{
-    fontSize:30,
-    fontStyle: 'bold',
+  title: {
+    fontSize: 30,
+    fontStyle: "bold",
   },
-  button:{
-    marginVertical:15,
-    paddingVertical:15,
-    paddingHorizontal:20,
+  button: {
+    marginVertical: 15,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
     borderRadius: 5,
-    backgroundColor:'#335ff2'
+    backgroundColor: "#335ff2",
   },
-  buttonText:{
-    color:'#fff',
-    fontSize:16,
-  }
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
 });
